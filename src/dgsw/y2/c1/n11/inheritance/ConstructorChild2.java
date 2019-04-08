c405b9da072937da669522c55e639a251a52d352
ShinMS <39144276+QMCHE@users.noreply.github.com>
2019-04-08 09:20:27 +0900

    Remove Files

--- src/dgsw/y2/c1/n11/inheritance/ConstructorChild2.java
+++
@@ -1,9 +0,0 @@
-package dgsw.y2.c1.n11.inheritance;
-
-public class ConstructorChild2 extends ConstructorParent2 {
-	public ConstructorChild2() {
-		super("Hello");
-		
-		System.out.println("자식 클래스의 생성자가 호출됨");
-	}
-}
--- src/dgsw/y2/c1/n11/inheritance/ConstructorParent1.java
+++
@@ -1,7 +0,0 @@
-package dgsw.y2.c1.n11.inheritance;
-
-public class ConstructorParent1 {
-	public ConstructorParent1() {
-		System.out.println("부모 클래스의 생성자가 호출됨");
-	}
-}
